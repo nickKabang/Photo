a4fa6f4ea6bbc313d8a9af384bea07eea52ccf84
kwg1031 <kwg1031@gmail.com>
2019-05-05 21:30:49 +0900

    photo

---
+++ app/src/main/java/com/example/photo/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.photo;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    public void mOnCaptureClick(View v)
+    {
+        String filename = "pippo.png";
+        String sd = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        File dest = new File(sd, filename);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cat);
+        try {
+            FileOutputStream out = new FileOutputStream(dest);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
